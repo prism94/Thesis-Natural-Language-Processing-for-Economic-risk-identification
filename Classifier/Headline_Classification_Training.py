02ffb34eb1d430e8dcf2fa422a74c88e3a181309
prism94 <34199108+prism94@users.noreply.github.com>
2021-08-15 15:52:25 +1000

    Add files via upload

---
+++ Classifier/Headline_Classification_Training.py
@@ -0,0 +1,69 @@
+import pickle
+import numpy as np
+import pandas as pd
+from gensim.models import word2vec
+
+
+max_padding = 5
+vec_length = 100
+
+with open(f'Word_Arrays.pkl', 'rb') as f:
+    training, testing = pickle.load(f)
+
+w2v = word2vec.Word2Vec.load('w2v')
+
+def sentence_to_sequence(model, text, vec_length):
+    splits = text.split(' ')
+    sequence = []
+    
+    for s in splits:
+        if s in model.wv:
+            sequence.append(model.wv[s].tolist())
+        else:
+            sequence.append([0 for i in range(vec_length)])
+    
+    return sequence
+
+def adjust_seq_length(seq, seq_length, vec_length):
+    _len_ = len(seq)
+    to_create = seq_length - _len_
+    if to_create < 0:
+        return np.NaN
+    else:
+        empties = [[0 for i in range(vec_length)] for n in range(to_create)]
+        return empties + seq
+
+
+training = pd.DataFrame(training)
+testing = pd.DataFrame(testing)
+
+training['wv'] = training[0].apply(lambda row: sentence_to_sequence(w2v, row, vec_length))
+testing['wv'] = testing[0].apply(lambda row: sentence_to_sequence(w2v, row, vec_length))
+
+training['length'] = training['wv'].apply(lambda x: len(x))
+
+max_length = training['length'].max()
+word_sequence_length = max_length + max_padding
+
+training['input'] = training['wv'].apply(lambda x: adjust_seq_length(x, word_sequence_length, vec_length))
+testing['input'] = testing['wv'].apply(lambda x: adjust_seq_length(x, word_sequence_length, vec_length))
+
+testing = testing[~testing['input'].isnull()]
+
+training[1] = training[1].astype(int)
+testing[1] = testing[1].astype(int)
+
+y_train = training[1].values
+
+X_train = training['input'].values.tolist()
+X_train = np.array(X_train)
+
+y_test = testing[1].values
+
+X_test = testing['input'].values.tolist()
+X_test = np.array(X_test)
+
+package = X_train, y_train, X_test, y_test
+
+with open('Modeling_Data.pkl', 'wb') as f:
+    pickle.dump(package, f)
---
+++ Classifier/Headline_Dataset_Preprocessing.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import pickle
+import string
+
+file_dir = 'C:/Users/User/OneDrive/Python_scripts/Thesis/Classifier'
+
+training_split = 0.75
+
+law = pd.read_csv('USNEWS.csv')
+bus = pd.read_csv('USNEWS_Business.csv')
+
+law_gl = pd.read_csv('GlobalNews.csv')
+bus_gl =  pd.read_csv('GlobalNews_Business.csv')
+
+
+def drop_lawsuits(bus, law):
+    bus['Overlap'] = False
+    for i in range(len(bus)):
+        date = bus['Date'].values[i]
+        head = bus['Headline'].values[i]
+        
+        lookup = law['Date'] == date
+        law_l = law[lookup]
+        
+        if len(law_l) > 0:
+            lookup = law_l['Headline'] == head
+            law_l = law_l[lookup]
+            
+            if len(law_l) > 0:
+                bus['Overlap'].values[i] = True
+    bus = bus[bus['Overlap'] == False]
+    del bus['Overlap']
+    return bus
+
+bus = drop_lawsuits(bus, law)
+bus_gl = drop_lawsuits(bus_gl, law_gl)
+
+def fix_text(text):
+    text=text.replace('Iâ€™', "'")
+    text=text.replace('â€™', "'")
+    
+    tab = str.maketrans("", "", string.punctuation)
+    return text.translate(tab)
+
+def label_data(a, lab):
+    df = pd.DataFrame(a)
+    df['Label'] = lab
+    return df.values
+
+bus['Headline'] = bus['Headline'].str.lower()
+bus_gl['Headline'] = bus_gl['Headline'].str.lower()
+
+bus['Headline'] = bus['Headline'].map(fix_text)
+bus_gl['Headline'] = bus_gl['Headline'].map(fix_text)
+
+
+bus_data = bus['Headline'].values
+bus_data = np.append(bus_data, bus_gl['Headline'].values)
+
+law['Headline'] = law['Headline'].str.lower()
+law_gl['Headline'] = law_gl['Headline'].str.lower()
+
+law['Headline'] = law['Headline'].map(fix_text)
+law_gl['Headline'] = law['Headline'].map(fix_text)
+
+law_data = law['Headline'].values
+law_data = np.append(law_data,  law_gl['Headline'].values)
+
+law_data = label_data(law_data, 1)
+bus_data = label_data(bus_data, 0)
+
+np.random.shuffle(law_data)
+np.random.shuffle(bus_data)
+
+training_law = law_data[:int(len(law_data)*training_split), :]
+testing_law = law_data[int(len(law_data)*training_split):, :]
+
+training_bus = bus_data[:int(len(bus_data)*training_split), :]
+testing_bus = bus_data[int(len(bus_data)*training_split):, :]
+
+
+training = np.array(training_law.tolist() + training_bus.tolist())
+testing = np.array(testing_law.tolist() + testing_bus.tolist())
+
+np.random.shuffle(training)
+np.random.shuffle(testing)
+
+package = training, testing
+
+with open(f'Word_Arrays.pkl', 'wb') as f:
+    pickle.dump(package, f)
